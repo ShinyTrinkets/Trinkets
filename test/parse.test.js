e91a1aba25ae171b4718f6c7cd1db8323ee7278f
Cristi Constantin <cristi.constantin@live.com>
2018-01-14 00:08:28 +0200

    Converting JSON blocks

--- test/parse.test.js
+++ test/parse.test.js
@@ -14,10 +14,18 @@ test('extracting json and yaml blocks correctly', async t => {
   t.deepEqual(blks, ['json\n[1, 2]', 'yaml\n- 1\n- 2'])
 })
 
+test('parsing js and json text correctly', async t => {
+  const txt = '\n# Hello\n\n```json // const x =\n{"a": 1, "b": 2}\n```\n\nSomething in between\n\n```js\nyes = true\n```\n\n## Good bye\n'
+  const blks = parse.extractBlocks(txt)
+  t.deepEqual(blks, ['json // const x =\n{"a": 1, "b": 2}', 'js\nyes = true'])
+  const c = await parse.convertText(txt)
+  t.is(c, 'const x = {"a": 1, "b": 2}\n\nyes = true')
+})
+
 test('parsing timer MD files correctly', async t => {
   const txt = '\n# Hello timer 🕰\n\nToday is a nice day...\n\n```js\ntrigger(\'timer\', \'*/20 * * * * *\', actions)\n```\n\nTomorrow will be even nicer.\n\n```js\nfunction actions (initial_value) {\n  console.log(\'Action ::\', initial_value)\n}\n```\n\n## Good bye ⏰\n'
   const c = await parse.convertText(txt)
-  t.deepEqual(c,
+  t.is(c,
     "trigger('timer', '*/20 * * * * *', actions)\n\n" +
     'function actions (initial_value) {\n  console.log(\'Action ::\', initial_value)\n}'
   )
