e9ebcefda8d95d7369b67dba520bb39661214860
Cristi Constantin <cristi.constantin@live.com>
2018-01-15 00:22:31 +0200

    Extracting meta from projects

--- test/parse.test.js
+++ test/parse.test.js
@@ -2,13 +2,20 @@
 import test from 'ava'
 import parse from '../lib/parse'
 
-test('extracting a js block correctly', async t => {
+test('extracting meta headers', t => {
+  const txt = '---\ntrinkets: true\nid: test1\ndb: true\n---\n\n# Hello\n\n'
+  const [meta, body] = parse.extractMeta(txt)
+  t.deepEqual(meta, { trinkets: true, id: 'test1', db: true })
+  t.is(body, '\n\n# Hello\n\n')
+})
+
+test('extracting a js block correctly', t => {
   const txt = '\nx = 1\n\n```js\na = 0\n```\n\ny = 2\n'
   const blks = parse.extractBlocks(txt)
   t.deepEqual(blks, ['js\na = 0'])
 })
 
-test('extracting json and yaml blocks correctly', async t => {
+test('extracting json and yaml blocks correctly', t => {
   const txt = '\nx = 1\n\n```json\n[1, 2]\n```\n\ny = 2\n\n```yaml\n- 1\n- 2\n```\n'
   const blks = parse.extractBlocks(txt)
   t.deepEqual(blks, ['json\n[1, 2]', 'yaml\n- 1\n- 2'])
@@ -18,23 +25,23 @@ test('parsing js and json text correctly', async t => {
   const txt = '\n# Hello !\n\n```json // const x =\n{"a": 1, "b": 2}\n```\n\nSomething in between\n\n```js\nyes = true\n```\n\n## Good bye\n'
   const blks = parse.extractBlocks(txt)
   t.deepEqual(blks, ['json // const x =\n{"a": 1, "b": 2}', 'js\nyes = true'])
-  const c = await parse.convertText(txt)
-  t.is(c, 'const x = {"a": 1, "b": 2}\n\nyes = true')
+  const c = parse.convertBlocks(txt)
+  t.is(c, 'const x = {"a": 1, "b": 2};\n\nyes = true;')
 })
 
 test('parsing js and yaml text correctly', async t => {
   const txt = '\n# Hello ?\n\n```js\nfunction () {\n  return x.a && x.b\n}\n```\n\nSomething in between\n\n```yaml // const x =\na: 1\nb: 2\n```\n\n## Good bye\n'
   const blks = parse.extractBlocks(txt)
   t.deepEqual(blks, ['js\nfunction () {\n  return x.a && x.b\n}', 'yaml // const x =\na: 1\nb: 2'])
-  const c = await parse.convertText(txt)
-  t.is(c, 'function () {\n  return x.a && x.b\n}\n\nconst x = {\n  "a": 1,\n  "b": 2\n}')
+  const c = parse.convertBlocks(txt)
+  t.is(c, 'function () {\n  return x.a && x.b\n};\n\nconst x = {\n  "a": 1,\n  "b": 2\n};')
 })
 
 test('parsing timer MD files correctly', async t => {
   const txt = '\n# Hello timer 🕰\n\nToday is a nice day...\n\n```js\ntrigger(\'timer\', \'*/20 * * * * *\', actions)\n```\n\nTomorrow will be even nicer.\n\n```js\nfunction actions (initial_value) {\n  console.log(\'Action ::\', initial_value)\n}\n```\n\n## Good bye ⏰\n'
-  const c = await parse.convertText(txt)
+  const c = parse.convertBlocks(txt)
   t.is(c,
-    "trigger('timer', '*/20 * * * * *', actions)\n\n" +
-    'function actions (initial_value) {\n  console.log(\'Action ::\', initial_value)\n}'
+    "trigger('timer', '*/20 * * * * *', actions);\n\n" +
+    'function actions (initial_value) {\n  console.log(\'Action ::\', initial_value)\n};'
   )
 })
